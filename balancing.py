1801e26d9faa265a87177ac8e1fd58a0e0d5181b
anajfalfa <anajfalfaiate@gmail.com>
2024-03-17 22:40:09 +0000

    report imagens confirmacao; balancing

---
+++ balancing.py
@@ -0,0 +1,79 @@
+from pandas import read_csv, concat, DataFrame, Series
+from matplotlib.pyplot import figure, show
+from dslabs_functions import plot_bar_chart
+
+file = "life_expectancy_ids_df_zscore_train"
+target = "Status"
+original: DataFrame = read_csv(f"data/{file}.csv", sep=",", decimal=".")
+
+target_count: Series = original[target].value_counts()
+positive_class = target_count.idxmin()
+negative_class = target_count.idxmax()
+'''
+print("Minority class=", positive_class, ":", target_count[positive_class])
+print("Majority class=", negative_class, ":", target_count[negative_class])
+print(
+    "Proportion:",
+    round(target_count[positive_class] / target_count[negative_class], 2),
+    ": 1",
+)
+values: dict[str, list] = {
+    "Original": [target_count[positive_class], target_count[negative_class]]
+}
+
+figure()
+plot_bar_chart(
+    target_count.index.to_list(), target_count.to_list(), title="Class balance"
+)
+show()'''
+
+
+'''df_positives: Series = original[original[target] == positive_class]
+df_negatives: Series = original[original[target] == negative_class]
+
+df_neg_sample: DataFrame = DataFrame(df_negatives.sample(len(df_positives)))
+df_under: DataFrame = concat([df_positives, df_neg_sample], axis=0)
+df_under.to_csv(f"data/{file}_under.csv", index=False)
+
+print("Minority class=", positive_class, ":", len(df_positives))
+print("Majority class=", negative_class, ":", len(df_neg_sample))
+print("Proportion:", round(len(df_positives) / len(df_neg_sample), 2), ": 1")
+'''
+from pandas import read_csv
+from numpy import array, ndarray
+from pandas import read_csv, DataFrame
+from numpy import ndarray
+from pandas import DataFrame, read_csv
+from matplotlib.pyplot import savefig, show, figure
+from dslabs_functions import plot_multibar_chart, CLASS_EVAL_METRICS, run_NB, run_KNN
+
+
+def evaluate_approach(
+    train: DataFrame, test: DataFrame, target: str = "class", metric: str = "accuracy"
+) -> dict[str, list]:
+    trnY = train.pop(target).values
+    trnX: ndarray = train.values
+    tstY = test.pop(target).values
+    tstX: ndarray = test.values
+    eval: dict[str, list] = {}
+
+    eval_NB: dict[str, float] = run_NB(trnX, trnY, tstX, tstY, metric=metric)
+    eval_KNN: dict[str, float] = run_KNN(trnX, trnY, tstX, tstY, metric=metric)
+    if eval_NB != {} and eval_KNN != {}:
+        for met in CLASS_EVAL_METRICS:
+            eval[met] = [eval_NB[met], eval_KNN[met]]
+    return eval
+
+
+target = "Status"
+file_tag = "zscore_under"
+train: DataFrame = read_csv("data/life_expectancy_ids_df_zscore_train_under.csv")
+test: DataFrame = read_csv("data/life_expectancy_ids_df_zscore_test.csv")
+
+figure()
+eval: dict[str, list] = evaluate_approach(train, test, target=target, metric="recall")
+plot_multibar_chart(
+    ["NB", "KNN"], eval, title=f"{file_tag} evaluation", percentage=True
+)
+savefig(f"images/{file_tag}_eval.png")
+show()
